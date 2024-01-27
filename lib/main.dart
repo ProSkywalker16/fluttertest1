a8261cc4061859f7e6dc9827e4100f050765fc88
Promit Chaudhuri <chaudhuripromit16@gmail.com>
2024-01-28 01:27:02 +0530

    modified:   android/app/src/main/AndroidManifest.xml 	new file:   android/app/src/main/res/mipmap-hdpi/launcher_icon.png 	new file:   android/app/src/main/res/mipmap-mdpi/launcher_icon.png 	new file:   android/app/src/main/res/mipmap-xhdpi/launcher_icon.png 	new file:   android/app/src/main/res/mipmap-xxhdpi/launcher_icon.png 	new file:   android/app/src/main/res/mipmap-xxxhdpi/launcher_icon.png 	new file:   images/icons8-car-50.png 	new file:   images/icons8-racing-car-48.png 	modified:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-1024x1024@1x.png 	modified:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@1x.png 	modified:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@2x.png 	modified:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@3x.png 	modified:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@1x.png 	modified:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@2x.png 	modified:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@3x.png 	modified:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@1x.png 	modified:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@2x.png 	modified:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@3x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-50x50@1x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-50x50@2x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-57x57@1x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-57x57@2x.png 	modified:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@2x.png 	modified:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@3x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-72x72@1x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-72x72@2x.png 	modified:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@1x.png 	modified:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@2x.png 	modified:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-83.5x83.5@2x.png 	modified:   lib/main.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyAppExt()
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyAppExt());
   }
 }
 
@@ -33,84 +29,86 @@ class MyAppExt extends StatefulWidget {
 class _MyAppExtState extends State<MyAppExt> {
   String buttonName = 'Click';
   int currentIndex = 0;
+  bool _isClicked = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blueAccent,
-          title: const Center(child: Text('App Test')),
-        ),
-        body: Center(
-
-          child: (currentIndex == 0)
-              ? Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  color: Color.fromARGB(255, 205, 12, 57),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Color.fromARGB(255, 246, 246, 246),
-                          backgroundColor: Color.fromARGB(255, 86, 224, 234),
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (BuildContext context) =>
-                                    const NextPage(),
-                              ),
-                            );
-                          });
-                        },
-                        child: const Text('Following Page'),
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: const Center(child: Text('App Test')),
+      ),
+      body: Center(
+        child: (currentIndex == 0)
+            ? Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Color.fromARGB(255, 205, 12, 57),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Color.fromARGB(255, 246, 246, 246),
+                        backgroundColor: Color.fromARGB(255, 86, 224, 234),
                       ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Color.fromARGB(255, 246, 246, 246),
-                          backgroundColor: Color.fromARGB(255, 11, 7, 2),
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            buttonName = 'LOL';
-                          });
-                        },
-                        child: Text(buttonName),
+                      onPressed: () {
+                        setState(() {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  const NextPage(),
+                            ),
+                          );
+                        });
+                      },
+                      child: const Text('Following Page'),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Color.fromARGB(255, 246, 246, 246),
+                        backgroundColor: Color.fromARGB(255, 11, 7, 2),
                       ),
-                    ],
-                  ),
-                )
-              : Image.asset('images/ferrariTestImage.jpg'),
-
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              label: 'Home',
-              icon: Icon(Icons.home /*, size: 30, color: Colors.lightBlue*/),
-            ),
-            BottomNavigationBarItem(
-                label: 'Settings',
-                icon: Icon(
-                    Icons.settings /*, size: 30, color: Colors.lightBlue*/))
-          ],
-          currentIndex: currentIndex,
-          onTap: (int index) {
-            setState(() {
-              currentIndex = index;
-            });
-          },
-        ),
-      );
+                      onPressed: () {
+                        setState(() {
+                          buttonName = 'LOL';
+                        });
+                      },
+                      child: Text(buttonName),
+                    ),
+                  ],
+                ),
+              )
+            : GestureDetector(
+                onTap: (){
+                  setState(() {
+                    _isClicked = !_isClicked;
+                  });
+                },
+                child: _isClicked ? Image.asset('images/ferrariTestImage.jpg'): Image.network('https://i.pinimg.com/736x/22/6e/55/226e552f4c084b9891d986231f39baec.jpg'),
+              ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home /*, size: 30, color: Colors.lightBlue*/),
+          ),
+          BottomNavigationBarItem(
+              label: 'Settings',
+              icon:
+                  Icon(Icons.settings /*, size: 30, color: Colors.lightBlue*/))
+        ],
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
   }
 }
-
-
-
-
 
 class NextPage extends StatelessWidget {
   const NextPage({super.key});
